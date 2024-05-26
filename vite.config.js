2c6612e9307a401185a5998b0360ec5f6ebbad60
Angel Fuentes <angelfc@csu.fullerton.edu>
2024-05-26 16:34:41 -0700

    GH Pages Testing Three

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // Ensure this matches your repository name
+  base: "", // Ensure the base path is set for GitHub Pages
 });
